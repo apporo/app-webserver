9d13f2d74889d0233485342afa21fa15f56caf9d
pnhung177 <pnhung177@drupalex.net>
2016-03-07 14:54:39 +0700

    The first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# app-webserver
+
+> Devebot app-webserver layerware
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var Devebot = require('devebot');
+
+module.exports = Devebot.attachLayer.bind(undefined, __dirname);
