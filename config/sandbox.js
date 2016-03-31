2155f01da63aee4259b1a77606169afea043a8c1
Pham Ngoc Hung <pnhung177@drupalex.net>
2016-03-31 16:45:09 +0700

    Load all basic middlewares in webserver plugin and provide ordering injection functionality to import other middlewares

--- config/sandbox.js
+++ config/sandbox.js
@@ -2,7 +2,14 @@ module.exports = {
   plugins: {
     appWebserver: {
       host: '0.0.0.0',
-      port: 7979
+      port: 7979,
+      session: {
+        name: 'sessionId',
+        secret: 'd0bi3td4y',
+        mongodb: {
+          uri: 'mongodb://localhost:27017/devebot-session'
+        }
+      }
     }
   }
 };
