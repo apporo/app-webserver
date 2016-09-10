0e74ec49eaba06e516081d5682845883bc8c9ed8
pnhung177 <pnhung177@acegik.net>
2016-09-10 16:28:35 +0700

    Append the cache-control header

--- config/sandbox.js
+++ config/sandbox.js
@@ -10,6 +10,15 @@ module.exports = {
           uri: 'mongodb://localhost:27017/devebot-session'
         }
       },
+      cacheControl: {
+        enabled: false,
+        pattern: {
+          operator: 'or',
+          url: /^\/(assets|css|js|picture|font)\/.+/,
+          contentType: /^\/(jpeg|png|gif)$/  
+        },
+        maxAge: 3600
+      },
       printRequestInfo: false
     }
   }
