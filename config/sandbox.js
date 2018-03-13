33e708400b5d57445f058229560d21cd8ff91a7c
pnhung177 <pnhung177@acegik.net>
2018-03-13 11:46:12 +0700

    Use babel-cli, babel-preset-env to build project

--- config/sandbox.js
+++ config/sandbox.js
@@ -2,27 +2,7 @@ module.exports = {
   plugins: {
     appWebserver: {
       host: '0.0.0.0',
-      port: 7979,
-      session: {
-        name: 'sessionId',
-        secret: 'd0bi3td4y',
-        store: {
-          type: 'redis',
-          url: 'redis://localhost:6379'
-        }
-      },
-      jsonBodySizeLimit: '1mb',
-      cacheControl: {
-        enabled: false,
-        pattern: {
-          operator: 'or',
-          url: /^\/(assets|css|js|picture|font)\/.+/,
-          contentType: /^\/(jpeg|png|gif)$/
-        },
-        maxAge: 3600
-      },
-      setPoweredBy: false,
-      printRequestInfo: false
+      port: 7979
     }
   }
 };
