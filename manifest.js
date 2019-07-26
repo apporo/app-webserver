9166e0c96d021255d2b41989b65b3c01577c704b
pnhung177 <pnhung177@acegik.net>
2019-07-26 22:07:49 +0700

    Add the manifest

---
+++ manifest.js
@@ -0,0 +1,56 @@
+module.exports = {
+  "config": {
+    "validation": {
+      "schema": {
+        "type": "object",
+        "properties": {
+          "enabled": {
+            "type": "boolean"
+          },
+          "host": {
+            "type": "string"
+          },
+          "port": {
+            "oneOf": [
+              {
+                "type": "number"
+              },
+              {
+                "type": "string"
+              }
+            ]
+          },
+          "ssl": {
+            "type": "object",
+            "properties": {
+              "enabled": {
+                "type": "boolean"
+              },
+              "ca": {
+                "type": "string"
+              },
+              "ca_file": {
+                "type": "string"
+              },
+              "key": {
+                "type": "string"
+              },
+              "key_file": {
+                "type": "string"
+              },
+              "cert": {
+                "type": "string"
+              },
+              "cert_file": {
+                "type": "string"
+              }
+            }
+          },
+          "verbose": {
+            "type": "boolean"
+          }
+        }
+      }
+    }
+  }
+};
